a526077f80a378d7ee50ae2178b16dfcdb8b5ba6
TuanAnh510 <nguyenhoangtuananh.nt16@gmail.com>
2023-08-24 20:04:29 +0700

    update

--- src/pages/balance.js
+++ src/pages/balance.js
@@ -1,5 +1,26 @@
-const Balance = () => {
-    return (<>Balance</>);
-}
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/flickity.css";
+import "../css/base.css";
+import "../css/home.css";
+import Navbar from "../component/Navbar/navbar";
+import Sidebar from "../component/Sidebar/sidebar";
+import Footer from "../component/Footer/footer";
 
-export default Balance;
+const balance = () => {
+  return (
+    <div class="container-wrapper">
+      <Navbar />
+      <main>
+        <Sidebar />
+        <div className="main-home">
+          <div className="main-content"></div>
+
+          <Footer />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default balance;
