57afa3a51ab8fa0932152c37639c5d1c3efb0d60
TuanAnh <nguyenhoangtuananh.nt16@gmail.com>
2023-08-31 14:31:17 +0700

    update

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -307,6 +307,7 @@ const Wallet = () => {
                                 </div>
                             ) : isOpenTransaction && (
                                 <Transaction />
+                                
                             )}
 
 
